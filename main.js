4b4b6022c77f66d89f5b1d06a6ad548e72e99a18
ivan <141366255+navisotas@users.noreply.github.com>
2025-04-09 08:29:43 -0300

    dfg

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ for (let i = 0; i < botoes.length; i++) {
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-12-05T00:00:00");
 const tempoObjetivo2 = new Date("2027-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2026-1-30T00:00:00");
+const tempoObjetivo3 = new Date("2026-01-30T00:00:00");
 const tempoObjetivo4 = new Date("2025-12-01T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
